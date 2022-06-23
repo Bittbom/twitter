07987a4f1600dbf61d32cda42a71a85b8bc984ab
Ryu Kwangchoon <67399705+Bittbom@users.noreply.github.com>
2022-06-23 12:48:37 +0900

    complet the signin pagee

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "twitter-9fae2.firebaseapp.com",
   projectId: "twitter-9fae2",
   storageBucket: "twitter-9fae2.appspot.com",
